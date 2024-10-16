20b1f00466f72cedfc487a22aa8febb66dc447ce
maskikuy <kproject305@gmail.com>
2024-10-16 20:26:52 +0700

    prak 2

--- prak2.py
+++ prak2.py
@@ -1,11 +1,14 @@
 def find_largest(a, b, c):
     return max(a, b, c)
 
-# Get input from user
+#Tentukan fungsinya
 angka_pertama = float(input("Enter first number: "))
 angka_kedua= float(input("Enter second number: "))
 angka_ketiga= float(input("Enter third number: "))
 
 # Find and print the largest number
 largest = find_largest(angka_pertama, angka_kedua, angka_ketiga)
+##Inisialisasi daftar kosong untuk menyimpan angka
 print(f"The largest number is: {largest}")
+## Bandingkan setiap angka dengan angka terbesar
+##Menampilkan hasil
